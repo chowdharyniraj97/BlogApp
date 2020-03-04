f47a89252378c880eb4d3a6e939ac9ed723eab55
NIRAJ CHOWDHARY <chowdharyniraj97@gmail.com>
2020-03-04 02:02:53 -0500

    added blog route

--- app.js
+++ app.js
@@ -19,10 +19,12 @@ var blogSchema=new mongoose.Schema({
 });
 var Blog=mongoose.model("Blog",blogSchema);
 
-Blog.create({
-    title: "test blog",
-    image: "https://images.unsplash.com/photo-1534361960057-19889db9621e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
-    body: "dog"
+app.get("/blogs",function(req,res){
+    res.render("index");
+});
+
+app.get("/",function(req,res){
+    res.send("welcome");
 });
 
 app.listen(3000,()=>{
